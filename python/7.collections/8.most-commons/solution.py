6c2ef33e0c93b86f5783b47c886125f3b9fd0899
Anil Bharadia <anil.b@ah.zymrinc.com>
2017-08-03 19:18:15 +0530

    working on the 'most-commons'

---
+++ python/7.collections/8.most-commons/solution.py
@@ -0,0 +1,12 @@
+from collections import OrderedDict, deque
+
+dq = deque(raw_input())
+
+od = OrderedDict()
+
+for c in dq:
+    if c in od:
+        od[c] += 1
+    else:
+        od[c] = 1
+print od
